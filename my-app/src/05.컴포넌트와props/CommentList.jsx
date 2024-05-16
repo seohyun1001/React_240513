dcdbaa709c6b22d9fbcb4c87739160db7e9ae346
seohyun1001 <sh54991210@gmail.com>
2024-05-16 12:13:23 +0900

    12:15 / 컴포넌트 댓글 실습2

--- my-app/src/05.컴포넌트와props/CommentList.jsx
+++ my-app/src/05.컴포넌트와props/CommentList.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name : "A",
+        comment : "hi",
+    },
+    {
+        name : "B",
+        comment : "nice to meet you",
+    },
+    {
+        name : "c",
+        comment : "have a good time",
+    },
+]
+
 
 function CommentList(props){
     return (
         <div>
-            <Comment name={"이인제"} comment={"안녕하세요"}/>
-            <Comment name={"유재석"} comment={"반갑습니다"}/>
+            {comments.map((comment) =>{
+                return(
+                    <Comment name={comment.name} comment={comment.comment} />
+                )
+            })}
         </div>
     )
 }
