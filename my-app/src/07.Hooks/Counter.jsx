d7f20430e3d956440411f32e2b31d76ae2a2b965
seohyun1001 <sh54991210@gmail.com>
2024-05-20 10:10:05 +0900

    10:10 / hook 사용 실습2

--- my-app/src/07.Hooks/Counter.jsx
+++ my-app/src/07.Hooks/Counter.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 function Counter(props){
-    var count = 0;
+    // var count = 0; 
+    const [count, setCount] = useState(0);
 
 
     return(
         <div>
             <p>총 {count}번 클릭했습니다.</p>
-            <button onClick={() => count++}>클릭</button>
+            <button onClick={() => setCount(count + 1)}>클릭</button>
             {/* -> 현재는 안됨 재랜더링이 안되고 있다는 뜻 */}
         </div>
     );
