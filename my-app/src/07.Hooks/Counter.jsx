7a162455a3eda0d5eec0b488ac83056a434909be
seohyun1001 <sh54991210@gmail.com>
2024-05-20 11:24:22 +0900

    11:20 / useReducer 사용 예시

--- my-app/src/07.Hooks/Counter.jsx
+++ my-app/src/07.Hooks/Counter.jsx
@@ -1,17 +1,28 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useReducer} from "react";
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'INCREMENT': return {value : state.value + 1};
+        case 'DECREMENT': return {value : state.value - 1};
+        default: return state;
+    }
+}
+
 function Counter(props){
     // var count = 0; 
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {value : 0});
 
-    useEffect(() => {
-        document.title = `총 ${count}번 클릭했습니다.`;
-    })
-
+    // useEffect(() => {
+    //     document.title = `총 ${count}번 클릭했습니다.`;
+    // })
 
     return(
         <div>
-            <p>총 {count}번 클릭했습니다.</p>
-            <button onClick={() => setCount(count + 1)}>클릭</button>
+            <p>현재 카운터 값은 <b>{state.value}</b>입니다.</p>
+            <button onClick={() => dispatch({type : 'INCREMENT'})}>+1</button>
+            <button onClick={() => dispatch({type : 'DECREMENT'})}>-1</button>
+            {/* <button onClick={() => setCount(count + 1)}>클릭</button> */}
             
         </div>
     );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -26,14 +26,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Library /> */}
     {/* <ConfirmDialog /> */}
     {/* <PropsEx /> */}
     {/* <CommentList /> */}
     {/* <BoardList /> */}
     {/* <NotificationList /> */}
-    {/* <Counter/> */}
+    <Counter/>
 
     {/* <Info /> */}
   </React.StrictMode>
