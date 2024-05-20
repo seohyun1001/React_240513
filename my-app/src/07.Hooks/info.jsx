480784a99b0f97a5b9467e754e55693ddd6495b7
seohyun1001 <sh54991210@gmail.com>
2024-05-20 10:53:31 +0900

    10:50 / Info에 useEffect 추가, App.js 수정

--- my-app/src/07.Hooks/info.jsx
+++ my-app/src/07.Hooks/info.jsx
@@ -7,9 +7,16 @@ function Info(props) {
     useEffect( () => {
         console.log("랜더링 완료");
         console.log({name, nickname});
-    }, [])
-    // 빈 배열을 넣으면 mount, unmount 시에 단 한 번씩만 실행됨
-    // 배열을 생략하면 랜더링이 바뀔 때마다 실행됨
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+            // 컴포넌트가 마운트 해제되기 전에 실행
+        }
+    })
+    // 컴포넌트가 마운트 된 이후,
+    // 의존성 배열에 있는 변수들 중 하나라도 값이 변경되었을 때 실행
+    // 빈 배열을 넣으면 mount, unmount 시에 단 한 번씩만 실행
+    // 배열을 생략하면 랜더링이 바뀔 때마다 실행
 
     const onChangeName = e => {
         setName(e.target.value);
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Info from './07.Hooks/info';
+import { useState } from 'react';
 
 function App() {
+
+  const [visible, serVisible] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div>
+      <button onClick={() => {
+        setVisible(!visible);
+      }}>
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr/>
+      {visible && <Info />}
+      {/* visible이 true 이면 Info 출력 */}
+      {/* visible이 false 이면 Info 미출력 */}
+      {/* -> if문으로 사용함 */}
     </div>
   );
 }
