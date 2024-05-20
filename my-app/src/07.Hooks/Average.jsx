8cb0b037df97eb2a5337beae53e3696ccf089f10
seohyun1001 <sh54991210@gmail.com>
2024-05-20 12:29:25 +0900

    12:30 / useCallback 사용

--- my-app/src/07.Hooks/Average.jsx
+++ my-app/src/07.Hooks/Average.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 const getAverage = numbers => {
     console.log("평균값 계산 중");
@@ -12,15 +12,26 @@ function Average(props) {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange = e => {
-        setNumber(e.target.value);
-    };
+    // const onChange = e => {
+    //     setNumber(e.target.value);
+    // };
 
-    const onInsert = e => {
+    // const onInsert = e => {
+    //     const nextList = list.concat(parseInt(number));
+    //     setList(nextList);
+    //     setNumber('');
+    // }
+
+    // useCallback : hook과 유사하지만 값이 아닌 함수를 반환
+    const onChange = useCallback(e => {
+        setNumber(e.target.value);
+    }, []); // 컴포넌트가 처음 랜더링 될 때만 함수 생성
+
+    const onInsert = useCallback(e => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    }
+    }, [number, list]); // number 혹은 list가 변경될 때만 함수 생성
 
     // useMemo 적용 전 : 숫자 입력과 등록에 getAverage() 함수 적용
     // -> 입력만 해도 랜더링이 됨
