84a333230cfb216ee160be1789944e6d407745af
seohyun1001 <sh54991210@gmail.com>
2024-05-30 10:06:05 +0900

    10:00 / 1교시 ~ 2.라우터 사용하기 중

---
+++ router_app/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About(props) {
+    return(
+        <div>
+            <h1>introduce</h1>
+            <p>practice react-router basic in this page</p>
+        </div>
+    )
+}
+
+export default About;
