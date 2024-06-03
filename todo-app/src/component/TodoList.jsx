3e62ed713d0052bc26509deb9ca671d1a7117490
seohyun1001 <sh54991210@gmail.com>
2024-06-03 09:19:56 +0900

    todo-app for reference

---
+++ todo-app/src/component/TodoList.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+import "./TodoList.css";
+
+const TodoList=({ todo, onUpdate, onDelete }) =>{
+    const [search, setSearch] = useState(""); 
+    const onChangeSearch = (e) => { // 
+        setSearch(e.target.value);
+    };
+    
+    const getSearchResult = () => {
+        return search === ""
+          ? todo
+          : todo.filter((it) =>
+              it.content.toLowerCase().includes(search.toLowerCase())
+            );
+    };
+    return(
+        <div className="TodoList">
+            <h4>Todo List 🔍</h4>
+            <input
+                value={search} 
+                onChange={onChangeSearch} 
+                className="searchbar" placeholder="검색어를 입력하세요" />
+            <div className="list_wrapper">
+                {getSearchResult().map((it) => ( 
+                    <TodoItem 
+                        key={it.id} {...it} 
+                        onUpdate={onUpdate}
+                        onDelete={onDelete} />
+                ))}
+                {/* {todo.map((it) => (
+                    
+                     <TodoItem key={it.id} {...it} />
+                ))} */}
+            </div>
+        </div>
+    );
+}
+export default TodoList;
