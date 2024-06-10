b57d285841a6b75192d4cbed594573f40ec7603d
seohyun1001 <sh54991210@gmail.com>
2024-06-10 09:50:09 +0900

    240610 9:50 / 비동기통신 news-app 실습

---
+++ news-app/src/App.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+  const onClick = async() => {
+    try {
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=acb96962bdb9478c8d301fa3a5c8cbcb')
+      setData(response.data);
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={50} cols={100} value={JSON.stringify(data,null,2)}/>}
+    </div>
+  );
+}
+
+export default App;
