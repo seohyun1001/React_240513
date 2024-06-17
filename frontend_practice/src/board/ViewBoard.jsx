03ee5f08fbbbcd2a9dc38fc7fb87937bf6583333
seohyun1001 <sh54991210@gmail.com>
2024-06-17 12:12:02 +0900

    240614 실습 추가분

--- frontend_practice/src/board/ViewBoard.jsx
+++ frontend_practice/src/board/ViewBoard.jsx
@@ -9,10 +9,19 @@ function ViewBoard() {
         title: '',
         content: '',
         writer: '',
+        hit: '',
+        create_time: '',
     });
 
-    const { title, content, writer } = board;
+    const { title, content, writer, hit } = board;
 
+    const [createTime, setCreateTime] = useState('');
+
+    useEffect(() => {
+        const currentDateTime = new Date();
+        const formattedDateTime = currentDateTime.toLocaleString(); // 또는 toLocaleDateString()을 사용할 수 있습니다.
+        setCreateTime(formattedDateTime);
+    }, []);
 
     const loadBoard = async () => {
         const result = await axios.get(`http://localhost:8082/board/${bno}`);
@@ -39,36 +48,46 @@ function ViewBoard() {
 
 
     return (
-        <div className="row">
-            <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                <div className="mb-3 text-center">
-                    <h2 className="text-center m-4">게시글</h2>
-
-                </div>
-
-                <form onSubmit={onSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="title" className="form-label">
-                            Title
-                        </label>
-                        <input onChange={onInputChange} value={title} type="text" id="title" className="form-control" name="title" readOnly/>
-                        <div style={{ marginBottom: '1rem' }}></div>
-
-                        <label htmlFor="content" className="form-label">
-                            Content
-                        </label>
-                        <input onChange={onInputChange} value={content} type="text" id="content" className="form-control" name="content" readOnly/>
-                        <div style={{ marginBottom: '1rem' }}></div>
-
-                        <label htmlFor="writer" className="form-label">
-                            Writer
-                        </label>
-                        <input onChange={onInputChange} value={writer} type="text" id="writer" className="form-control" name="writer" readOnly/>
+        <div class="row mt-3">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        게시글
                     </div>
+                    <div class="card-body">
+                        <form onSubmit={onSubmit}>
+                            <div className="input-group mb-3">
+                            <span class="input-group-text">Title</span>
+                                <input onChange={onInputChange} value={title} type="text" id="title" className="form-control" name="title" readOnly />
+                                <div style={{ marginBottom: '1rem' }}></div>
+                            </div>
 
-                </form>
-                <Link to={`/editboard/${board.bno}`} type="submit" className="btn btn-outline-primary px-3 mx-2">수정</Link>
-                <Link to="/" type="submit" className="btn btn-outline-dark px-3 mx-2">목록</Link>
+                            <div className="input-group mb-3">
+                            <span class="input-group-text">Content</span>
+                                <input onChange={onInputChange} value={content} type="text" id="content" className="form-control" name="content" readOnly />
+                                <div style={{ marginBottom: '1rem' }}></div>
+                            </div>
+
+                            <div className="input-group mb-3">
+                            <span class="input-group-text">writer</span>
+                                <input onChange={onInputChange} value={writer} type="text" id="writer" className="form-control" name="writer" readOnly />
+                            </div>
+
+                            <div className="input-group mb-3">
+                                <span class="input-group-text">조회수</span>
+                                <input onChange={onInputChange} value={hit} type="text" id="hit" className="form-control" name="hit" readOnly />
+                            </div>
+
+                            <div className="input-group mb-3">
+                                <span class="input-group-text">등록 날짜</span>
+                                <input onChange={onInputChange} value={createTime} type="text" id="create_time" className="form-control" name="create_time" readOnly />
+                            </div>
+
+                        </form>
+                        <Link to={`/editboard/${board.bno}`} type="submit" className="btn btn-outline-primary px-3 mx-2">수정</Link>
+                        <Link to="/" type="submit" className="btn btn-outline-dark px-3 mx-2">목록</Link>
+                    </div>
+                </div>
             </div>
         </div>
 
