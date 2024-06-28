8cce1bec8f5e7980100c4ca378393aea68ce15f1
seohyun1001 <sh54991210@gmail.com>
2024-06-28 10:14:40 +0900

    240628 10:15 / redux 예제

---
+++ redux-app/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+
+function App() {
+
+  const dispatch = useDispatch()
+
+  const { value } = useSelector(state => state.value)
+  const { count } = useSelector(state => state.count)
+
+  const addValue = () => {
+    dispatch({ type: 'increment' })
+  }
+
+  const subValue = () => {
+    dispatch({ type: 'decrement'})
+  }
+
+  const resetValue = () => {
+    dispatch({ type: 'reset' })
+  }
+
+  const pushButton = () => {
+    dispatch({ type: 'push' })
+  }
+
+  return (
+    <div className="App">
+      <div>
+        value: {value}
+      </div>
+
+      <button onClick={addValue}> + </button>
+      <button onClick={subValue}> - </button>
+      <button onClick={resetValue}> reset </button>
+
+      <div>
+        count : {count}
+      </div>
+
+      <buttton onClick={pushButton}> click </buttton>
+
+    </div>
+  );
+}
+
+export default App;
